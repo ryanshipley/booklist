0a4a55df254d9cb6d3dc6d9d68a767acc3332b6d
Ryan Shipley <ryanshipley23@gmail.com>
2022-09-06 12:11:15 -0400

    creates books

--- server.js
+++ server.js
@@ -2,24 +2,25 @@
 const express = require('express');
 const app = express();
 const mongoose = require("mongoose");
+const Book = require("./models/books.js");
 require('dotenv').config();
 
-mongoose.connect(process.env.DATABASE_URI, {
+const db = mongoose.connection
+mongoose.connect(process.env.DATABASE_URL, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true
 });
 
-const db = mongoose.connection
+
 db.on('error', (err) => console.log(err.message + ' is mongo not running?'));
 db.on('connected', () => console.log('mongo connected'));
 db.on('disconnected', () => console.log('mongo disconnected'));
 
 app.use(express.urlencoded({ extended: true}));
 
-// Listener
-const PORT = process.env.PORT;
-app.listen(PORT, () => console.log(`server is listening on port: ${PORT}`));
+// ROUTES
 
+// CREATE
 app.post('/books', (req, res) => {
 	if (req.body.completed === 'on') {
 		//if checked, req.body.completed is set to 'on'
@@ -28,5 +29,16 @@ app.post('/books', (req, res) => {
 		//if not checked, req.body.completed is undefined
 		req.body.completed = false;
 	}
-	res.send(req.body);
+    
+	Book.create(req.body, (error, createdBook) => {
+		res.send(createdBook);
+	});
 });
+
+app.get('/books/new', (req, res) => {
+	res.send('new');
+});
+
+// Listener
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`server is listening on port: ${PORT}`));
