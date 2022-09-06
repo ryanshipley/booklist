6bba2da940924652b56bbf4305a6b066e54c16c5
Ryan Shipley <ryanshipley23@gmail.com>
2022-09-06 05:15:49 -0400

    tests postman

--- server.js
+++ server.js
@@ -14,10 +14,12 @@ db.on('error', (err) => console.log(err.message + ' is mongo not running?'));
 db.on('connected', () => console.log('mongo connected'));
 db.on('disconnected', () => console.log('mongo disconnected'));
 
+app.use(express.urlencoded({ extended: true}));
+
 // Listener
 const PORT = process.env.PORT;
-app.listen(PORT, () => console.log(`server is listning on port: ${PORT}`));
+app.listen(PORT, () => console.log(`server is listening on port: ${PORT}`));
 
-app.get("/books", (req, res) =>{
-    res.send("received");
+app.post("/books", (req, res) =>{
+    res.send(req.body);
 });
